f0b1b44ce8fd34ba3224d743b1668ac3645aeac6
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2025-09-29 23:55:25 +0200

    created js updated html and css

---
+++ src/index.js
@@ -0,0 +1,47 @@
+function updateLosAngelesTime() {
+   //Los angeles
+   const losAngEl = document.querySelector("#los-angeles");
+   const losAngDateEl = losAngEl.querySelector(".date");
+   const losAngTimeEl = losAngEl.querySelector(".time");
+   const losAngTime = moment().tz("America/Los_Angeles");
+
+   losAngDateEl.innerHTML = moment().format("MMMM Do YYYY");
+   losAngTimeEl.innerHTML = `${losAngTime.format(
+      "h:mm:ss"
+   )} <span class="ampm">${losAngTime.format("A")}</span>`;
+}
+
+setInterval(updateLosAngelesTime, 1000);
+
+//Paris
+const parisEl = document.querySelector("#paris");
+const parisDateEl = parisEl.querySelector(".date");
+const parisTimeEl = parisEl.querySelector(".time");
+const parisTime = moment().tz("Europe/Paris");
+
+parisDateEl.innerHTML = parisTime.format("MMMM Do YYYY");
+parisTimeEl.innerHTML = `${parisTime.format(
+   "h:mm:ss"
+)} <span class="ampm">${parisTime.format("A")}</span>`;
+
+//Sydney
+const sydneyEl = document.querySelector("#sydney");
+const sydneyDateEl = sydneyEl.querySelector(".date");
+const sydneyTimeEl = sydneyEl.querySelector(".time");
+const sydneyTime = moment().tz("Australia/Sydney");
+
+sydneyDateEl.innerHTML = sydneyTime.format("MMMM Do YYYY");
+sydneyTimeEl.innerHTML = `${sydneyTime.format(
+   "h:mm:ss"
+)} <span class="ampm">${sydneyTime.format("A")}</span>`;
+
+//Tokyo
+const tokyoEl = document.querySelector("#tokyo");
+const tokyoDateEl = tokyoEl.querySelector(".date");
+const tokyoTimeEl = tokyoEl.querySelector(".time");
+const tokyoTime = moment().tz("Asia/Tokyo");
+
+tokyoDateEl.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeEl.innerHTML = `${tokyoTime.format(
+   "h:mm:ss"
+)} <span class="ampm">${tokyoTime.format("A")}</span>`;
